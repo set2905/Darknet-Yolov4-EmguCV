592e8ced391b588f8f06a4cc7efcd4b5ead3260a
set2905 <72752613+set2905@users.noreply.github.com>
2022-05-05 13:45:06 +0300

    Enum Binding

---
+++ WPFYOLO/WPFYOLO/EnumBindingSourceExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace WPFYOLO
+{
+    public class EnumBindingSourceExtension : MarkupExtension
+    {
+        public Type EnumType { get; private set; }
+
+        public EnumBindingSourceExtension(Type enumType)
+        {
+            if (enumType is null || !enumType.IsEnum)
+                throw new Exception("incorrect EnumType or EnumType is null");
+            EnumType = enumType;
+        }
+        public override object ProvideValue(IServiceProvider _)
+        {
+            return Enum.GetValues(EnumType);
+        }
+
+
+    }
+}
